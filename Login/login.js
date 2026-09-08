bb12e700d565807a082febf3cb20e95aae330b1b
Neyton Duarte <neyton.santos@edu.senai.br>
2026-09-08 14:56:56 -0300

    Terminado tela login

--- Login/login.js
+++ Login/login.js
@@ -1,7 +1,30 @@
+// var nome = "Késsia"
+// const nome = "Késsia"
+
+// let nome = "Késsia";
+
+// console.log(nome);
+
+// nome = "Gustavo"
+// console.log(nome);
+
+
+// const nome = "Késsia"
+// console.log(nome);
+
+// nome = "Gustavo"
+// console.log(nome);
 const campoSenha = document.getElementById("senha");
 const btnSenha = document.querySelector("#mostrar-senha");
 
 btnSenha.addEventListener("click", function () {
-    alert("Senha: " + campoSenha.value);
-})
+
+    // if (campoSenha.type == "password") {
+    //     campoSenha.type = "text";
+    // } else {
+    //     campoSenha.type = "password";
+    // }
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password";
+});
+
 
